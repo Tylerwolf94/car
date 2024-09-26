03f783f26e7d1fe538f33e770b85269391d6bd88
centy <caongare@gmail.com>
2024-09-26 17:07:17 +0300

    update cart deletion

--- cart.js
+++ cart.js
@@ -1,28 +1,66 @@
 let cart = [];
 let total = 0;
 
+const cartItems = document.getElementById('cart-items');
+
 function addToCart(itemName, itemPrice) {
-    cart.push({ name: itemName, price: itemPrice });
+    const len = cart.length+1;
+    cart.push({ name: itemName, price: itemPrice, id: len });
     total += itemPrice;
-    updateCartDisplay();
+
+    const li = document.createElement('li');
+    li.innerHTML = `${itemName} - Ksh ${itemPrice.toFixed(2)}`; // Display price in Kshs
+
+    const btn = document.createElement('button');
+    li.setAttribute('id', `cart-button-${len}`)
+
+    btn.innerHTML = 'x';
+    li.appendChild(btn);
+
+    btn.addEventListener('click', ()=> removeElem(`cart-button-${len}`, itemPrice))
+
+    cartItems.appendChild(li);
+
+    document.getElementById('cart-count').textContent = cart.length;
+    document.getElementById('total-price').textContent = `Total: Ksh ${total.toFixed(2)}`;
+
     document.getElementById('cart').style.display = 'block'; // Show cart when item added
 }
 
-function updateCartDisplay() {
-    const cartItems = document.getElementById('cart-items');
-    cartItems.innerHTML = '';
-    
-    // Display each item in the cart
-    cart.forEach(item => {
-        const li = document.createElement('li');
-        li.textContent = `${item.name} - Ksh ${item.price.toFixed(2)}`; // Display price in Kshs
-        cartItems.appendChild(li);
-    });
+const removeElem = (id, price) => {
+    const item = document.getElementById(id);
+    total -= price;
 
-    // Update cart count and total price
+    if(item) cartItems.removeChild(item);
+
+    document.getElementById('total-price').textContent = `Total: Ksh ${total.toFixed(2)}`;
     document.getElementById('cart-count').textContent = cart.length;
-    document.getElementById('total-price').textContent = `Total: Ksh ${total.toFixed(2)}`; // Display total in Kshs
+
 }
+
+
+const items = cartItems.getElementsByClassName
+
+const deleteItem = (item) => {
+    // const updated = cart.filter(i=> i.name !== item.name);
+    const lis = cartItems.getElementsByTagName('li');
+    const lists = [...lis];
+    lists.forEach((l, i)=> {
+        if(l.innerHTML.includes(item.name)){
+            lists.splice(i, 1)
+        }
+    })
+    cartItems.innerHTML = lists;
+
+    // updated.forEach(newItem => {
+    //     const li = document.createElement('li');
+    //     li.textContent = `${newItem.name} - Ksh ${newItem.price.toFixed(2)}`;
+    //     cartItems.appendChild(li);
+    // })
+
+
+}
+
 
 function closeCart() {
     document.getElementById('cart').style.display = 'none';
